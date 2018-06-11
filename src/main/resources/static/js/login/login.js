f8e9d0aa711d524044a2ad271d0996b1826cee60
lgq <lgq@localhost.localdomain>
2018-06-11 17:00:32 +0800

    update

--- src/main/resources/static/js/login/login.js
+++ src/main/resources/static/js/login/login.js
@@ -1,27 +1,31 @@
-
 var Login = (function () {
 
     function Login() {
 
     }
 
-    Login.prototype.checkOn = function() {
-        var prevUrl = $("#prevUrl").val();
+    Login.prototype.checkOn = function () {
+        var username =$("input[name=username]").val().trim();
+        var password =$("input[name=password]").val().trim();
+        if(username=="" || password==""){
+            return;
+        }
         var formData = $("#login_form").serialize();
+        var prevUrl = $("#prevUrl").val();
         $.ajax({
             type: "POST",
             dataType: "json",
-            url: "/login" ,
+            url: "/login",
             data: formData,
             success: function (rs) {
-                if(rs.code==0){
-                    localStorage.setItem("token",rs.data.token)
-                    window.location.href=prevUrl; //跳转到上一个未登陆页
-                }else {
+                if (rs.code == 0) {
+                    localStorage.setItem("token", rs.data.token)
+                    window.location.href = prevUrl; //跳转到上一个未登陆页
+                } else {
                     $("#msg").text(rs.msg);
                 }
             },
-            error : function() {
+            error: function () {
                 alert("异常！");
             }
         });
